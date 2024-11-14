4adff33cb437fd6603f54932476d211b99f67bbb
0xNooodle <0xNoodle@proton.me>
2024-11-14 13:19:58 +0100

    Update license

--- src/core/abstracts/MarketStorage.sol
+++ src/core/abstracts/MarketStorage.sol
@@ -1,4 +1,4 @@
-// SPDX-License-identifier: BUSL-1.1
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.27;
 
 import { Ownable2Step } from "@openzeppelin/contracts/access/Ownable2Step.sol";
