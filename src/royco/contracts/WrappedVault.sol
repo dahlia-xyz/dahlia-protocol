677d7670d12bedd31456ce4552a600916ba27095
0xNooodle <0xNoodle@proton.me>
2024-12-06 15:12:38 +0100

    Update testTakeRewards()

--- src/royco/contracts/WrappedVault.sol
+++ src/royco/contracts/WrappedVault.sol
@@ -544,7 +544,12 @@ contract WrappedVault is Ownable, InitializableERC20, IWrappedVault {
         RewardsInterval memory rewardsInterval = _rewardToInterval[reward];
         if (rewardsInterval.start > block.timestamp || block.timestamp >= rewardsInterval.end) return 0;
 
-        uint256 rewardsRate = (uint256(rewardsInterval.rate) * assets * 1e18 / (totalPrincipal() + assets)) / assets;
+        // Initial version
+        uint256 rewardsRate = (uint256(rewardsInterval.rate) * assets / (totalPrincipal() + assets)) * 1e18 / assets;
+
+        // The fix for issue #2
+        //uint256 rewardsRate = (uint256(rewardsInterval.rate) * assets * 1e18 / (totalPrincipal() + assets))  / assets;
+
         // Account for interest rate accrued in Dahlia market
         if (reward == address(DEPOSIT_ASSET)) {
             uint256 dahliaRate = dahlia.previewLendRateAfterDeposit(marketId, assets);
