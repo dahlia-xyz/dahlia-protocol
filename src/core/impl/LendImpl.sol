10c57104d747915d46fcf26587ac226954ca8bcb
ivan7kov7 <ivan7kov7@gmail.com>
2024-12-18 11:01:23 +0200

    improve gas Overall gas change: -5831

--- src/core/impl/LendImpl.sol
+++ src/core/impl/LendImpl.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.27;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import { SafeCastLib } from "@solady/utils/SafeCastLib.sol";
 import { Errors } from "src/core/helpers/Errors.sol";
 import { SharesMathLib } from "src/core/helpers/SharesMathLib.sol";
 import { IDahlia } from "src/core/interfaces/IDahlia.sol";
@@ -13,7 +12,6 @@ import { IDahlia } from "src/core/interfaces/IDahlia.sol";
 library LendImpl {
     using SafeERC20 for IERC20;
     using SharesMathLib for uint256;
-    using SafeCastLib for uint256;
 
     function internalLend(IDahlia.Market storage market, IDahlia.UserPosition storage ownerPosition, uint256 assets, address owner)
         internal
@@ -21,8 +19,8 @@ library LendImpl {
     {
         shares = assets.toSharesDown(market.totalLendAssets, market.totalLendShares);
 
-        ownerPosition.lendShares += shares.toUint128();
-        ownerPosition.lendPrincipalAssets += assets.toUint128();
+        ownerPosition.lendShares += uint128(shares);
+        ownerPosition.lendPrincipalAssets += uint128(assets);
         market.totalLendPrincipalAssets += assets;
         market.totalLendShares += shares;
         market.totalLendAssets += assets;
@@ -42,7 +40,7 @@ library LendImpl {
             revert Errors.InsufficientLiquidity(market.totalBorrowAssets, totalLendAssets);
         }
         ownerLendShares = ownerPosition.lendShares - shares;
-        ownerPosition.lendShares = ownerLendShares.toUint128();
+        ownerPosition.lendShares = uint128(ownerLendShares);
         market.totalLendShares = totalLendShares - shares;
         market.totalLendAssets = totalLendAssets;
 
