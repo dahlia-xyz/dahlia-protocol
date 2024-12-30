02e879a88baf2f38bf05936761d73ec145c47cfb
Javier Hayek <javier.hayek@proton.me>
2024-12-30 16:52:09 +0100

    remove dahlшadeterministic

--- scripts/setup-contracts.ts
+++ scripts/setup-contracts.ts
@@ -72,7 +72,7 @@ const deployContracts = async (rpcPort: string, otterscanPort: string): Promise<
   await $$({
     cwd: "..",
     env: {
-      PRIVATE_KEY: "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+      PRIVATE_KEY: private_key,
       DAHLIA_OWNER: "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
       DAHLIA_PRIVATE_KEY: "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",
       FEES_RECIPIENT: "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC",
