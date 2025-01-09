6456db5705bf994f6704e382ac3a1fc446efde32
ivan7kov7 <ivan7kov7@gmail.com>
2025-01-09 21:42:11 +0200

    cleanup

--- scripts/envs.ts
+++ scripts/envs.ts
@@ -1,7 +1,5 @@
 import Network from "./network.ts";
 
-// TODO: Make Logs write to file
-
 export const DEPLOY_NETWORKS: Network[] = [Network.CARTIO];
 
 // WARNING! ENABLE THIS ONCE YOU HAVE TESTED ON DOCKER!!!
