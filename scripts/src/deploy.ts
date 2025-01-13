f125c52570bf6758f37e20259e11962b82e89848
ivan7kov7 <ivan7kov7@gmail.com>
2025-01-13 21:00:25 +0200

    use --script instead of --file

--- scripts/src/deploy.ts
+++ scripts/src/deploy.ts
@@ -5,10 +5,10 @@ import { deployContractsOnNetworks } from "../utils.ts";
 const program = new Command();
 
 program
-  .requiredOption("-f, --file <path>", "Path to the .s.sol file")
+  .requiredOption("-s, --script <path>", "Path to the .s.sol file")
   .option("-i, --iterator <name>", "Name of the iterator from config.yaml")
   .parse(process.argv);
 
 const options = program.opts();
 
-await deployContractsOnNetworks(options.file, options.iterator);
+await deployContractsOnNetworks(options.script, options.iterator);
