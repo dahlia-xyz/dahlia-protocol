6cbb6eb578a3f7feba187b508a8f085a04ba01e5
ivan7kov7 <ivan7kov7@gmail.com>
2025-01-14 16:45:35 +0200

    create ChainlinkCompatWstETHToETHPriceFeed

--- src/oracles/contracts/DahliaWstEthETHOracle.sol
+++ src/oracles/contracts/DahliaWstEthETHOracle.sol
@@ -2,92 +2,76 @@
 pragma solidity ^0.8.27;
 
 import { AggregatorV3Interface } from "@chainlink/contracts/v0.8/shared/interfaces/AggregatorV3Interface.sol";
-import { Ownable, Ownable2Step } from "@openzeppelin/contracts/access/Ownable2Step.sol";
-import { FixedPointMathLib } from "@solady/utils/FixedPointMathLib.sol";
-import { AggregatorV3InterfaceLib } from "src/oracles/abstracts/AggregatorV3InterfaceLib.sol";
+import { IERC20Metadata } from "@openzeppelin/contracts/interfaces/IERC20Metadata.sol";
 import { Errors } from "src/oracles/helpers/Errors.sol";
-import { IDahliaOracle } from "src/oracles/interfaces/IDahliaOracle.sol";
 
-/// @title DahliaWstEthEthOracle
+/// @dev copied used methods from https://github.com/lidofinance/core/blob/master/contracts/0.6.12/WstETH.sol
+interface IWstETH is IERC20Metadata {
+    /**
+     * @notice Get amount of stETH for a one wstETH
+     * @return Amount of stETH for 1 wstETH
+     */
+    function stEthPerToken() external view returns (uint256);
+}
+
+/// @title ChainlinkCompatWstETHToETHPriceFeed
 /// @notice Oracle to convert wstETH-ETH using a stETH-ETH Chainlink feed.
-contract DahliaWstEthEthOracle is IDahliaOracle, Ownable2Step {
-    using FixedPointMathLib for uint256;
-    using AggregatorV3InterfaceLib for AggregatorV3Interface;
+/// @dev adapted from https://github.com/lidofinance/wsteth-eth-price-feed/blob/main/contracts/AAVECompatWstETHToETHPriceFeed.sol
+contract ChainlinkCompatWstETHToETHPriceFeed is AggregatorV3Interface {
+    error UnsupportedMethod();
+    error BadWstETHToStETH();
 
     struct Params {
         address wstEth;
         AggregatorV3Interface stEthToEthFeed;
     }
 
-    struct Delays {
-        uint256 stEthToEthMaxDelay;
-    }
-
-    event MaximumOracleDelayUpdated(Delays newMaxDelays);
     event ParamsUpdated(Params params);
 
-    uint256 public immutable ORACLE_PRECISION;
-    address public immutable WST_ETH;
+    uint8 internal immutable _DECIMAL;
+    int256 internal immutable _WST_ETH_PRECISION;
+    IWstETH public immutable WST_ETH;
     AggregatorV3Interface public immutable STETH_TO_ETH_FEED;
 
-    uint256 internal stEthToEthMaxDelay;
+    uint256 public stEthToEthMaxDelay;
 
-    constructor(address owner, Params memory params, Delays memory delays) Ownable(owner) {
+    constructor(Params memory params) {
         require(params.wstEth != address(0), Errors.ZeroAddress());
         require(address(params.stEthToEthFeed) != address(0), Errors.ZeroAddress());
 
-        WST_ETH = params.wstEth;
+        WST_ETH = IWstETH(params.wstEth);
         STETH_TO_ETH_FEED = params.stEthToEthFeed;
+        _DECIMAL = STETH_TO_ETH_FEED.decimals();
+        _WST_ETH_PRECISION = int256(10 ** WST_ETH.decimals());
 
         emit ParamsUpdated(params);
-        _setMaximumOracleDelays(delays);
-
-        // Precision scaling factor for prices
-        ORACLE_PRECISION = 1e36;
     }
 
-    function _setMaximumOracleDelays(Delays memory delays) private {
-        emit MaximumOracleDelayUpdated({ newMaxDelays: delays });
-        stEthToEthMaxDelay = delays.stEthToEthMaxDelay;
+    function decimals() external view returns (uint8) {
+        return _DECIMAL;
     }
 
-    function setMaximumOracleDelays(Delays memory delays) external onlyOwner {
-        _setMaximumOracleDelays(delays);
+    function description() external pure returns (string memory) {
+        return "WSTETH / ETH";
     }
 
-    function maxDelays() external view returns (Delays memory) {
-        return Delays({ stEthToEthMaxDelay: stEthToEthMaxDelay });
+    function version() external pure returns (uint256) {
+        return 1;
     }
 
-    /// @inheritdoc IDahliaOracle
-    function getPrice() external view override returns (uint256, bool) {
-        return _getWstEthInEth();
+    function getRoundData(uint80) external pure returns (uint80, int256, uint256, uint256, uint80) {
+        revert UnsupportedMethod();
     }
 
-    /// @dev Internal function to compute wstETH-ETH in e36 precision
-    ///  1. Read stETH->ETH price from Chainlink feed
-    ///  2. Cap the stETH->ETH price if necessary
-    ///  3. Compute tokensPerStEth from wstETH
-    ///  4. Compute price wstETH->ETH in e36 precision
-    /// @return price The computed price
-    /// @return isBadData Boolean indicating if the data is stale or invalid
-    function _getWstEthInEth() internal view returns (uint256 price, bool isBadData) {
-        (uint256 stEthToEthPrice, bool stEthToEthBad) = STETH_TO_ETH_FEED.getFeedPrice(stEthToEthMaxDelay);
+    function latestRoundData() external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound) {
+        int256 wstETHToStETH = int256(WST_ETH.stEthPerToken());
+        require(wstETHToStETH > 0, BadWstETHToStETH());
 
-        if (stEthToEthPrice > ORACLE_PRECISION) {
-            stEthToEthPrice = ORACLE_PRECISION;
-        }
-
-        uint256 tokensPerStEth = IWstETH(WST_ETH).tokensPerStEth();
-        isBadData = stEthToEthBad;
-
-        uint256 wstEthInEth_1e18 = FixedPointMathLib.mulDiv(stEthToEthPrice, 1e18, tokensPerStEth);
-        price = wstEthInEth_1e18 * 1e18; // Scale to e36 precision
-
-        return (price, isBadData);
+        (roundId, answer, startedAt, updatedAt, answeredInRound) = STETH_TO_ETH_FEED.latestRoundData();
+        // TODO: do we need this?
+        //        if (answer > 10 ** _decimal) {
+        //            answer = 10 ** _decimal;
+        //        }
+        answer = answer * wstETHToStETH / _WST_ETH_PRECISION;
     }
-}
-
-interface IWstETH {
-    function tokensPerStEth() external view returns (uint256);
 }
