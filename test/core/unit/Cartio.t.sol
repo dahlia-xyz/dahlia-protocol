8995312ca3da090ab9ff1146c089eb79a3a3b533
ivan7kov7 <ivan7kov7@gmail.com>
2025-01-15 11:04:07 +0200

    Investigate getPositionLTV

--- test/core/unit/Cartio.t.sol
+++ test/core/unit/Cartio.t.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.27;
 import { Vm } from "@forge-std/Test.sol";
 import { console } from "@forge-std/console.sol";
 import { FixedPointMathLib } from "@solady/utils/FixedPointMathLib.sol";
+
+import { Constants } from "src/core/helpers/Constants.sol";
 import { MarketMath } from "src/core/helpers/MarketMath.sol";
 import { SharesMathLib } from "src/core/helpers/SharesMathLib.sol";
 import { IDahlia } from "src/core/interfaces/IDahlia.sol";
@@ -20,9 +22,10 @@ contract CartioTest is DahliaTest {
 
     TestContext ctx;
     TestContext.MarketContext $;
+    IDahlia dahlia = IDahlia(0x0A7e67A977cf9aB1DE3781Ec58625010050E446E);
 
     function setUp() public {
-        vm.createSelectFork("cartio", 3_792_585);
+        vm.createSelectFork("cartio", 4_374_811);
         ctx = new TestContext(vm);
     }
 
@@ -44,7 +47,6 @@ contract CartioTest is DahliaTest {
 
     /// run with `forge test -vv --mt test_cartio_getMaxBorrowableAmount`
     function test_cartio_getMaxBorrowableAmount() public {
-        IDahlia dahlia = IDahlia(0x0A7e67A977cf9aB1DE3781Ec58625010050E446E);
         IDahlia.MarketId id = IDahlia.MarketId.wrap(1);
         IDahlia.Market memory market = dahlia.getMarket(id);
         console.log("totalBorrowAssets", market.totalBorrowAssets);
@@ -54,5 +56,23 @@ contract CartioTest is DahliaTest {
         console.log("borrowedAssets", borrowedAssets);
         console.log("borrowableAssets", borrowableAssets);
         console.log("collateralPrice", collateralPrice);
+    }
+
+    function _getPositionLTV(IDahlia.Market memory market, IDahlia.UserPosition memory position) internal view returns (uint256 ltv) {
+        uint256 collateralPrice = MarketMath.getCollateralPrice(market.oracle);
+        console.log("collateralPrice", collateralPrice);
+        uint256 borrowedAssets = position.borrowShares.toAssetsUp(market.totalBorrowAssets, market.totalBorrowShares);
+        console.log("borrowedAssets", borrowedAssets);
+        uint256 totalCollateralCapacity = MarketMath.collateralToLendUp(position.collateral, collateralPrice);
+        console.log("totalCollateralCapacity", totalCollateralCapacity);
+        return totalCollateralCapacity == 0 ? 0 : borrowedAssets.mulDivUp(Constants.LLTV_100_PERCENT, totalCollateralCapacity);
+    }
+
+    function test_getPositionLTV() public {
+        IDahlia.MarketId id = IDahlia.MarketId.wrap(1);
+        address user = 0xd940909AE50e084706e479604BAe660b5F932E18;
+        IDahlia.Market memory marketData = dahlia.getMarket(id);
+        IDahlia.UserPosition memory position = dahlia.getPosition(id, user);
+        _getPositionLTV(marketData, position);
     }
 }
